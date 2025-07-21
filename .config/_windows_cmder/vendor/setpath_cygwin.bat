e291edf7346ebe94d679b82117b4408c902104ac
scillidan <scillidan@yandex.com>
2025-07-21 14:40:25 +0000

    Merge branch 'main' of https://github.com/scillidan/dotfiles

--- .config/_windows_cmder/vendor/setpath_cygwin.bat
+++ .config/_windows_cmder/vendor/setpath_cygwin.bat
@@ -2,4 +2,6 @@
 
 SET PATH=^
 %    =%C:\Users\User\Usr\Git\dotfiles\.config\_windows_cmder\vendor;^
+%    =%C:\cygwin64\bin;^
+%    =%C:\cygwin64\sbin;^
 %    =%%PATH%
--- .config/_windows_cmder/vendor/setpath_w64devkit.bat
+++ .config/_windows_cmder/vendor/setpath_w64devkit.bat
@@ -2,4 +2,5 @@
 
 SET PATH=^
 %    =%C:\Users\User\Usr\Git\dotfiles\.config\_windows_cmder\vendor;^
+%    =%C:\gcc-lua-install\w64devkit\bin;^
 %    =%%PATH%
