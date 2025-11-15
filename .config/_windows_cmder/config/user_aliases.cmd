e7e2ad71d38f561b9622de059c33f3c6b3596ec1
scillidan <scillidan@yandex.com>
2025-11-15 22:57:17 +0800

    update

--- .config/_windows_cmder/config/user_aliases.cmd
+++ .config/_windows_cmder/config/user_aliases.cmd
@@ -11,64 +11,19 @@ pwd=cd
 clr=clear
 ipi=ipconfig | findstr /i "ipv4"
 
-;= rem OS
-gunzip=gzip
-
-;= Rem Lib
-;= :: python
-py310=%SCOOP_HOME%\apps\python310\current\python.exe $*
-pv310=%SCOOP_HOME%\apps\python310\current\python.exe -m venv .venv && .venv\Scripts\activate.bat
-pi310=%SCOOP_HOME%\apps\python310\current\python.exe -m pip install $*
-pu310=%SCOOP_HOME%\apps\python310\current\python.exe -m pip uninstall $*
-py311=%SCOOP_HOME%\apps\python311\current\python.exe $*
-pv311=%SCOOP_HOME%\apps\python311\current\python.exe -m venv .venv && .venv\Scripts\activate.bat
-py312=%SCOOP_HOME%\apps\python312\current\python.exe $*
-pv312=%SCOOP_HOME%\apps\python312\current\python.exe -m venv .venv && .venv\Scripts\activate.bat
-pva=.venv\Scripts\activate.bat
-ps=pip_search $*
-
-;= :: pipx
-xpi=pipx install $*
-xpu=pipx uninstall $*
-
-;= :: uv
-uv310=uv venv --python 3.10 && .venv\Scripts\activate.bat
-uva=.venv\Scripts\activate.bat
-uvv=uv venv $1 --python 3.10 && $1\Scripts\activate.bat
-uvi=uv pip install $*
-
-;= :: npm
-ns=npms search $*
-
-;= :: pnpm
-xni=%SCOOP_HOME%\shims\pnpm.exe add -g $*
-xnu=%SCOOP_HOME%\shims\pnpm.exe remove -g $*
-xnl=%SCOOP_HOME%\shims\pnpm.exe ls -g
-
-;= :: cargo
-cgi=cargo install --force $*
-cgu=cargo uninstall $*
-cgs=cargo search $* --registry crates-io
-
-;= :: CUDA
-cuda11=set "CUDA_PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8"
-cuda12=set "CUDA_PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.3"
-
-;= rem Opt
-e=explorer $CD$\$*
-d=dopus "%CD%\$*"
-
-;= rem Shell
-rfe=%USERPROFILE%\Usr\Shell\RefrEnv\refrenv.bat
-git-sync=%BASH% %USERPROFILE%\Usr\Shell\git-sync\git-sync $*
-mgit=%BASH% %USERPROFILE%\Usr\Shell\multi-git-status\mgitstatus
-ugit=%BASH% %USERPROFILE%\Usr\Shell\ugit\ugit
-pdf2jpg=%BASH% C:\Users\User\Usr\Git\Shell\_arch\pdf2jpg.sh $*
-
 ;= rem Bin
 mlbin=mklink %USERPROFILE%\.local\bin\$2 %CD%\$1
 
-;= :: scoop
+;= rem choco
+ch=choco info $*
+cs=choco search $*
+ci=gsudo choco install -y $*
+cu=gsudo choco uninstall $*
+cup=gsudo choco upgrade $*
+cua=gsudo choco upgrade all -y
+cl=choco list
+
+;= rem scoop
 ss=scoop search $*
 si=scoop install $*
 su=scoop uninstall $*
@@ -80,31 +35,22 @@ sua=scoop update -a -k
 scca=scoop cache rm *
 scua=scoop cleanup *
 
-;= :: choco
-ch=choco info $*
-cs=choco search $*
-ci=gsudo choco install -y $*
-cu=gsudo choco uninstall $*
-cup=gsudo choco upgrade $*
-cua=gsudo choco upgrade all -y
-cl=choco list
-
-;= :: gh extensions
+;= rem gh extensions
 ghtd=gh tidy
 gcorg=gh clone-org $*
 ghdl=gh download $*
 
-;= :: alass
+;= rem alass
 alas=alass $1.$2 $1.$3 $1.alass.$3
 
-;= :: doggo
+;= rem doggo
 dga=doggo A $* @1.1.1.1
 dgg=doggo A $* @8.8.8.8
 
-;= :: erd
+;= rem erd
 et=erd --color auto --hidden --follow --human --sort name --dir-order first --layout inverted $*
 
-;= :: eza
+;= rem eza
 ls=eza $*
 l=eza --git-ignore $*
 ll=eza --all --header --long $*
@@ -114,9 +60,64 @@ lx=eza -lbhHigUmuSa@
 lt=eza --tree $*
 tree=eza --tree $*
 
-;= :: gopencc
+;= rem gopencc
 t2c=gopencc -c t2s -i $*
 c2t=gopencc -c s2t -i $*
 
-;= :: jq
+;= rem jq
 jqp=jq ".scripts" package.json
+
+;= rem lstr
+tree=lstr
+
+;= rem Lib
+;= rem cargo
+cgi=cargo install --force $*
+cgu=cargo uninstall $*
+cgs=cargo search $* --registry crates-io
+
+;= rem npm
+ns=npms search $*
+;= rem pnpm
+xni=%SCOOP_HOME%\shims\pnpm.exe add -g $*
+xnu=%SCOOP_HOME%\shims\pnpm.exe remove -g $*
+xnl=%SCOOP_HOME%\shims\pnpm.exe ls -g
+
+;= rem python
+py310=%SCOOP_HOME%\apps\python310\current\python.exe $*
+pv310=%SCOOP_HOME%\apps\python310\current\python.exe -m venv .venv && .venv\Scripts\activate.bat
+pi310=%SCOOP_HOME%\apps\python310\current\python.exe -m pip install $*
+pu310=%SCOOP_HOME%\apps\python310\current\python.exe -m pip uninstall $*
+py311=%SCOOP_HOME%\apps\python311\current\python.exe $*
+pv311=%SCOOP_HOME%\apps\python311\current\python.exe -m venv .venv && .venv\Scripts\activate.bat
+py312=%SCOOP_HOME%\apps\python312\current\python.exe $*
+pv312=%SCOOP_HOME%\apps\python312\current\python.exe -m venv .venv && .venv\Scripts\activate.bat
+pva=.venv\Scripts\activate.bat
+ps=pip_search $*
+;= rem pipx
+xpi=pipx install $*
+xpu=pipx uninstall $*
+;= rem uv
+uv310=uv venv --python 3.10 && .venv\Scripts\activate.bat
+uva=.venv\Scripts\activate.bat
+uvv=uv venv $1 --python 3.10 && $1\Scripts\activate.bat
+uvi=uv pip install $*
+
+;= rem Opt
+;= rem CUDA
+cuda11=set "CUDA_PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8"
+cuda12=set "CUDA_PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.3"
+
+;= rem OS
+gunzip=gzip
+
+;= rem Shell
+rfe=%USERPROFILE%\Usr\Shell\RefrEnv\refrenv.bat
+git-sync=%BASH% %USERPROFILE%\Usr\Shell\git-sync\git-sync $*
+mgit=%BASH% %USERPROFILE%\Usr\Shell\multi-git-status\mgitstatus
+ugit=%BASH% %USERPROFILE%\Usr\Shell\ugit\ugit
+pdf2jpg=%BASH% C:\Users\User\Usr\Git\Shell\_arch\pdf2jpg.sh $*
+
+;= rem Other
+d=dopus "%CD%\$*"
+e=explorer $CD$\$*
--- .config/_windows_cmder/config/user_profile.cmd
+++ .config/_windows_cmder/config/user_profile.cmd
@@ -6,6 +6,7 @@ set "CHANGE_LOG_NAME=User"
 set "SYS_ARCH=64"
 
 rem Lib
+rem ---
 set "PYENV=%USERPROFILE%\.pyenv\pyenv-win"
 set "PYENV_ROOT=%USERPROFILE%\.pyenv\pyenv-win"
 set "PYENV_HOME=%USERPROFILE%\.pyenv\pyenv-win"
@@ -15,10 +16,11 @@ set "RBENV_USE_MIRROR=CN"
 set "RBENV_ROOT=%USERPROFILE%\Usr\Lib\rbenv"
 
 rem Lib for Opt
+rem ---
 set "NODE_OPTIONS=--max_old_space_size=2048"
-:: set "NODE_ENV=development node postcss"
-:: set "PUPPETEER_SKIP_DOWNLOAD=true"
-:: set "PUPPETEER_PRODUCT=firefox npm install"
+rem set "NODE_ENV=development node postcss"
+rem set "PUPPETEER_SKIP_DOWNLOAD=true"
+rem set "PUPPETEER_PRODUCT=firefox npm install"
 set "PUPPETEER_EXECUTABLE_PATH=%LocalAppData%\ms-playwright\chromium-1117\chrome-win\chrome.exe"
 set "DENO_INSTALL=%LocalAppData%\deno"
 set "PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python"
@@ -26,38 +28,43 @@ set "RUST_BACKTRACE=full"
 set "RUSTFLAGS=--cfg tracing_unstable"
 set "CARGO_HTTP_CHECK_REVOKE=false"
 set "GOSUMDB=sum.golang.org"
-:: set "ANDROID_NDK_HOME=%USERPROFILE%\Usr\Lib\android-ndk"
+rem set "ANDROID_NDK_HOME=%USERPROFILE%\Usr\Lib\android-ndk"
 
 rem CUDA
-:: set "CUDNN=%CUDNN%"
+rem ---
+rem set "CUDNN=%CUDNN%"
 set "HF_MODEL_ROOT=C:\huggingface\model"
 set "HF_DATASET_ROOT=C:\huggingface\dataset"
 set "HF_MIRROR=https://hf-mirror.com"
-:: set "HF_MIRROR=https://huggingface.co"
-:: set "HF_ENDPOINT=https://hf-mirror.com"
+rem set "HF_MIRROR=https://huggingface.co"
+rem set "HF_ENDPOINT=https://hf-mirror.com"
 set "HF_HUB_ENABLE_HF_TRANSFER=1"
 set "NVCC_FLAGS=-allow-unsupported-compiler"
-:: set "LD_LIBRARY_PATH=TensorRT\8.5.1.7\lib"
+rem set "LD_LIBRARY_PATH=TensorRT\8.5.1.7\lib"
 set "_use_new_zipfile_serialization=False"
 
 rem Bin
-:: clink-terminal
+rem ---
+rem clink-terminal
 set "CLINK_FZF_PREVIEW_SIXELS=1"
 
-rem Opt
-set "FAST_REFRESH=true"
+rem lstr
+set "LS_COLORS=rs=0:di=01;34:ln=01;36:ex=01;32:*.zip=01;31:*.png=01;35:"
 
-rem OptWeb
-:: kokoro-tts-cli
-set "KOKORO_PATH=%USERPROFILE%\Usr\OptWeb\Kokoro-TTS-Local"
-
-:: alass-cli
+rem alass-cli
 set "ALASS_FFMPEG_PATH=%SCOOP_HOME%\apps\ffmpeg\current\bin\ffmpeg.exe"
 set "ALASS_FFPROBE_PATH=%SCOOP_HOME%\apps\ffmpeg\current\bin\ffprobe.exe"
 
-:: espeak-ng
+rem espeak-ng
 set "PHONEMIZER_ESPEAK_LIBRARY=%SCOOP_HOME%\apps\espeak-ng\current\eSpeak NG\libespeak-ng.dll"
 set "PHONEMIZER_ESPEAK_PATH=%USERPROFILE%\Git\cli\espeak-ng.bat"
+
+rem kokoro-tts-cli
+set "KOKORO_PATH=%USERPROFILE%\Usr\OptWeb\Kokoro-TTS-Local"
+
+rem Opt
+rem ---
+set "FAST_REFRESH=true"
 
 call "%CMDER_ROOT%\vendor\setpath.bat"
 call "%USERPROFILE%\Usr\Shell\RefrEnv\refrenv.bat"
