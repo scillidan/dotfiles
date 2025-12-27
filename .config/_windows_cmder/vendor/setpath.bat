bf3bc9d3c17f126a7e66cf2273469f0d38be1296
scillidan <scillidan@yandex.com>
2025-12-27 20:44:01 +0800

    update

--- .config/_windows_cmder/vendor/setpath.bat
+++ .config/_windows_cmder/vendor/setpath.bat
@@ -2,10 +2,10 @@
 
 SET PATH=^
 %    =%%USERHOME%\Usr\Source\clink\clink-fzf;^
+%    =%%USERHOME%\Usr\Source\clink\clink-terminal\bin;^
 %    =%%PATH%
 
 SET CLINK_PATH=^
 %    =%%USERHOME%\Usr\Source\clink\clink-zoxide;^
-%    =%%USERHOME%\Usr\Source\clink\clink-terminal\bin;^
 %    =%%USERHOME%\Usr\Source\clink\clink-terminal\scripts;^
 %    =%%CLINK_PATH%
