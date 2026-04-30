2b3caac5d76be9d431ad3edfd99647e526dee554
scillidan <scillidan@yandex.com>
2026-04-30 09:47:51 +0800

    update

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -6,6 +6,16 @@ require("custom-shell"):setup({
 	history_path = "default",
 	save_history = true,
 })
+require("git"):setup({
+	order = 1500,
+})
+require("font-sample"):setup({
+	text = 'ABCDEF abcdef\n0123456789 \noO08 iIlL1 g9qCGQ\n8%& <([{}])>\n.,;: @#$-_="\n== <= >= != ffi\nâéùïøçÃĒÆœ\n및개요これ直楽糸',
+	canvas_size = "750x800",
+	font_size = 80,
+	bg = "white",
+	fg = "black",
+})
 
 if is_unix then
 	if not is_termux then
