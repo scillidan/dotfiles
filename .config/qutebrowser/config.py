6d6d0ed7acdb272bfbe0a3ca841d23a904e4ecff
scillidan <scillidan@yandex.com>
2026-07-04 18:20:36 +0800

    .

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,4 +1,16 @@
 config.load_autoconfig()
+
 config.source("themes/city-lights-theme.py")
 c.fonts.default_family = "Sarasa Term SC Nerd"
+c.scrolling.bar = "never"
+c.zoom.default = "80%"
+
+c.auto_save.session = True
+c.session.lazy_restore = True
 c.downloads.location.directory = "~/Downloads"
+
+c.aliases['ob'] = 'open -t -- {clipboard}'
+c.aliases['ss'] = 'screenshot ~/Downloads/screenshot-$(date +%s).png'
+
+config.bind(',gc', 'spawn --userscript git-clone')
+config.bind(',rr', 'spawn --userscript repomix-remote')
