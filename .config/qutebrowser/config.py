e6cc6dcaf43ec346d19781d2dd529ca277347f90
scillidan <scillidan@yandex.com>
2026-07-06 09:14:23 +0800

    .

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,8 +1,14 @@
 import os
 userhome = os.environ.get('USERHOME', os.path.expanduser('~'))
-downloads_dir = os.path.join(userhome, 'Downloads')
+downloads_dir = os.path.join(userhome, '/Downloads')
 
-config.load_autoconfig()
+config.load_autoconfig(False)
+
+c.content.cookies.accept = "all"
+c.content.cookies.store = True
+c.session.default_name = "default"
+c.session.lazy_restore = True
+c.auto_save.session = True
 
 config.source("themes/city-lights-theme.py")
 c.fonts.default_family = "Sarasa Term SC Nerd"
@@ -10,14 +16,12 @@ c.window.hide_decoration = True
 c.scrolling.bar = "never"
 c.zoom.default = "80%"
 
-c.auto_save.session = True
-c.session.lazy_restore = True
-c.downloads.location.directory = os.path.join(userhome, '/Downloads')
+c.downloads.location.directory = downloads_dir
 c.downloads.location.suggestion = 'filename'
 c.downloads.location.remember = False
 
 c.aliases['ob'] = 'open -t -- {clipboard}'
-c.aliases['ss'] = 'screenshot ~/{downloads_dir}/screenshot-$(date +%s).png'
+c.aliases['ss'] = f'screenshot {downloads_dir}/screenshot-$(date +%s).png'
 
 config.bind(',gc', 'spawn --userscript git-clone')
 config.bind(',rr', 'spawn --userscript repomix-remote')
